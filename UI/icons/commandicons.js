39d4d9093d05eab965aca61416258f7f8a120717
Aidenthebest6789 <Sawyeraiden4@gmail.com>
2025-11-14 15:15:56 -0500

    Remove SSRRIcon from commandicons.js
    Removed SSRRIcon from command icons.

--- UI/icons/commandicons.js
+++ UI/icons/commandicons.js
@@ -4,7 +4,6 @@ const cmdIcons = {
     msgIcon : 'https://cdn.discordapp.com/emojis/915167284648116244.png',
     rippleIcon : 'https://cdn.discordapp.com/emojis/903301195307843595.gif',
     levelUpIcon : 'https://cdn.discordapp.com/emojis/908223980438163476.gif',
-    SSRRIcon : 'https://cdn.discordapp.com/emojis/1334648756649590805.png',
     PollIcon : 'https://i.ibb.co/j9ddStvs/polling.png',
 };
 
