b52dcff89171d2ef35ecaba227083b3436036b1c
Aidenthebest6789 <Sawyeraiden4@gmail.com>
2025-11-14 16:16:55 -0500

    Update lavalink configuration details

--- lavalink.js
+++ lavalink.js
@@ -2,10 +2,11 @@
 module.exports = {
     enabled: true, 
     lavalink: {
-      name: "GlaceYT",
-      password: "glace",
+      name: "owbzgr1k",
+      password: "G9ehOVbom7f",
       host: "87.106.62.92",
       port:  11642,
       secure: false
     }
 };
+
